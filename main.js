9011f47f9e9179adf259ccd2e8532abfdcc6ab4a
masta-mind <123703378+masta-mind@users.noreply.github.com>
2023-01-27 03:24:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const aliceTumbling = [
+  { transform: 'rotate(0) scale(1)' },
+  { transform: 'rotate(360deg) scale(0)' }
+];
+
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: 'forwards'
+}
+
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+
+// My work from here:
+
+async function sequence() {
+  try {
+    await alice1.animate(aliceTumbling, aliceTiming).finished;
+    await alice2.animate(aliceTumbling, aliceTiming).finished;
+    alice3.animate(aliceTumbling, aliceTiming)
+  }
+  catch(error){
+    console.error(`Could not play the animation: ${error}`)
+  }
+} 
+
+sequence();
